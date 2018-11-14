b12861fca8bee3fa234a09e825461cd58ecc67cf
Lam Hieu <lamhieu.vk@gmail.com>
2018-11-15 02:11:06 +0700

    Update package config

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "lamhieu:unblock",
-  version: "0.0.2",
+  version: "0.0.3",
   summary: "This package provides `this.unblock` inside Meteor publications",
   git: "https://github.com/lamhieu-vk/meteor-unblock.git",
   documentation: "README.md",
